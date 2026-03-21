c543dbcc130094c4b8c214dfa80f324b5727ccf4
Patrick <patrick@mpls.dev>
2026-03-22 00:16:08 +0100

    fix: add build/ to gitignore, remove build artifacts

--- build/CMakeFiles/qitto.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for qitto.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/patrick/Webs/ditto
-# Build directory: /home/patrick/Webs/ditto/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_crc32 "/home/patrick/Webs/ditto/build/test_crc32")
-set_tests_properties(test_crc32 PROPERTIES  _BACKTRACE_TRIPLES "/home/patrick/Webs/ditto/CMakeLists.txt;88;add_test;/home/patrick/Webs/ditto/CMakeLists.txt;0;")
