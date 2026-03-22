c02c7bc1100c55e29a8bc7e2c09c3a7e18cd6846
Patrick <patrick@mpls.dev>
2026-03-22 01:10:24 +0100

    debug: trace ClipListModel SQL query and eof status

--- linux/ClipListModel.cpp
+++ linux/ClipListModel.cpp
@@ -65,7 +65,9 @@ void ClipListModel::refresh(const QString &searchText)
                        "LIMIT %d", (const char*)search, m_pageSize + 1);
         }
 
+        fprintf(stderr, "[ClipListModel] SQL: %s\n", (const char*)sql);
         CppSQLite3Query q = GetDittoDB().execQuery(sql);
+        fprintf(stderr, "[ClipListModel] query executed, eof=%d\n", q.eof());
 
         while (q.eof() == false) {
             ClipListItem item;
